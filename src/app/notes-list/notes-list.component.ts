2cc2754a5c77ab5e2f4b4e35019536215549bdb9
isurugamage37 <isurugamage37@gmail.com>
2023-11-05 18:24:30 -0500

    Initial commit

--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -54,14 +54,14 @@ export class NotesListComponent implements OnInit {
         let currentDate = (new Date().getHours() > 12? new Date().getHours() - 12: new Date().getHours()) + ':'  + new Date().getMinutes() + (new Date().getHours() > 12? ' PM': ' AM');    
         
         this.notesList.forEach(note => note.selected = false);
-        this.notesList.unshift({
-          id: Math.random() * 100,
-          body: '',
-          title: '',
-          date: currentDate,
-          selected: true
-        });
-        this.noteClickHandler(this.notesList[0]);
+        // this.notesList.push({
+        //   id: Math.random() * 100,
+        //   body: '',
+        //   title: '',
+        //   date: currentDate,
+        //   selected: true
+        // });
+        this.noteClickHandler(this.notesList[this.notesList,length-1]);
       }
       localStorage.setItem('notes', JSON.stringify(this.notesList));
     });
